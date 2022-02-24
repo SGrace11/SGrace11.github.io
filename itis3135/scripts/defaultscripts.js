e0bfb5832777796bc76fc18f8721357c463e8bab
SGrace11 <sgrace8@uncc.edu>
2022-02-24 17:26:56 -0500

    Updating First JS

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -2,3 +2,10 @@ function scriptTest()
 {
     alert("Running Script");
 }
+
+//function getInfo()
+//{
+//    let firstname = document.getElementById("firstname").value;
+//    let feeling = document.getElementById("feeling").value;
+//    document.getElementById("output").innerHTML = feeling;
+//}
