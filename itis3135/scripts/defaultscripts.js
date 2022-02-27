016dd7bad2461abec793574196d2a49eae67248d
SGrace11 <sgrace8@uncc.edu>
2022-02-27 18:33:38 -0500

    Update defaultscripts.js

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -15,8 +15,14 @@ function getDateTime()
 {
     let statement = "Today is ";
     let date = new Date();
+    let dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
     statement += date.getHours().toString();
-    statement += ":";
-    statement += date.getMinutes().toString();
+    statement += ":" + date.getMinutes().toString();
+    statement += " on ";
+    statement += dayNames[date.getDay()];
+    statement += ", " + date.getDate().toString();
+    statement += " " + monthNames[date.getMonth()];
+    statement += ", " + date.getFullYear().toString();
     document.getElementById("date").innerHTML = statement;
 }
