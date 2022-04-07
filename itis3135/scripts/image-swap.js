57d9cea7208836a1ad46efd7379f225dd3fdb5b2
SGrace11 <sgrace8@uncc.edu>
2022-04-07 17:11:05 -0400

    Work on image swap page and JS

--- itis3135/scripts/image-swap.js
+++ itis3135/scripts/image-swap.js
@@ -1,1 +1,24 @@
 let imageIndex = 1;
+
+function showImage(n)
+{
+    let i;
+    let images = document.getElementsByClassName("image");
+    let captionText = document.getElementById("caption");
+
+    if(n > images.length)
+    {
+        imageIndex = 1;
+    }
+    if(n < 1)
+    {
+        imageIndex = slides.length;
+    }
+
+    for(i = 0; i < slides.length; i++)
+    {
+        images[i].style.display = "none";
+    }
+    images[imageIndex - 1].style.display = "block";
+    captionText.innerHTML = images[imageIndex - 1].alt;
+}
