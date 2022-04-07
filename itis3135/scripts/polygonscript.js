cc380e59cd63a7304fc64b116a290a71813c6fff
SGrace11 <sgrace8@uncc.edu>
2022-04-07 18:48:36 -0400

    Work on polygons

--- itis3135/scripts/polygonscript.js
+++ itis3135/scripts/polygonscript.js
@@ -1,19 +1,47 @@
-function getShape(_sideCount)
+function polygon()
 {
-    let sideCount = _sideCount;
-    
+    let sideCount = parseInt(prompt("Enter a number of sides."));
+    let validatedSideCount = validateEntry(sideCount);
+    let results = getShape(validatedSideCount);
+    alert(results);
 }
 
-function validateEntry()
+function getShape(_sideCount)
 {
-    let entry = prompt("Enter a number of sides.");
-    if(Number.isInteger(parseInt(entry)) != true)
+    let shapeName;
+    
+    if(_sideCount = 1)
+    {shapeName = monogon;}
+    else if(_sideCount = 2)
+    {shapeName = digon;}
+    else if(_sideCount = 3)
+    {shapeName = triangle;}
+    else if(_sideCount = 4)
+    {shapeName = quadrilateral;}
+    else if(_sideCount = 5)
+    {shapeName = pentagon;}
+    else if(_sideCount = 6)
+    {shapeName = hexagon;}
+    else if(_sideCount = 7)
+    {shapeName = septagon;}
+    else if(_sideCount = 8)
+    {shapeName = octagon;}
+    else if(_sideCount = 9)
+    {shapeName = nonagon;}
+    else if(_sideCount = 10)
+    {shapeName = decagon;}
+
+    return shapeName;
+}
+
+function validateEntry(_entry)
+{
+    let entry = _entry;
+    while(isNaN(entry) || entry < 1 || entry > 10)
     {
         alert("Hey, that's not an integer!");
         entry = prompt("Enter a number of sides.");
     }
-    else
-    {
-        getShape(parseInt(entry));
-    }
+    result = getShape(entry);
+    return result;
 }
