184afabc7472f92c488670017abeea55baa1159d
SGrace11 <sgrace8@uncc.edu>
2022-04-21 18:56:05 -0400

    Work on arrays

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -3,7 +3,8 @@ const salaries = [];
 
 function addSalary()
 {
-
+    let employee = document.getElementById('employees').value;
+    let salary = document.getElementById('salary').value;
 }
 
 function displayResults()
