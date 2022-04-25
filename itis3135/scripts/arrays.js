28c72aae8f876ae8ce453ece2d36c4bd1d5d2c31
SGrace11 <sgrace8@uncc.edu>
2022-04-24 22:21:21 -0400

    Work on arrays functions

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -5,14 +5,43 @@ function addSalary()
 {
     let employee = document.getElementById('employees').value;
     let salary = document.getElementById('salary').value;
+
+    if(isNaN(salary) || salary=="")
+    {
+        alert("Please enter a number for the salary");
+    } else
+    {
+        people.push(employee);
+        salaries.push(salary);
+    }
 }
 
 function displayResults()
 {
+    var average = 0;
+    var highest = salaries[0];
+    var count = 0;
 
+    for(count; count < salaries.length; count++)
+    {
+        average += salaries[count];
+        if(salaries[count] > highest)
+        {
+            highest = salaries[count];
+        }
+    }
+    average = average/count;
+
+    document.getElementById('results').innerHTML = "<h2>Salary Results</h2><p>Average: " + average + "</p><p>Highest: "+ highest + "</p>";
 }
 
 function displaySalary()
 {
-    
+    var table = "<tr><th>Name</th><th>Monthly Salary</th></tr>";
+
+    for(var i = 0; i < people.length; i++)
+    {
+        table += "<tr><td>" + people[i] + "</td><td>" + salaries[i] + "</td></tr>";
+    }
+    document.getElementById('results_table').innerHTML = table;
 }
