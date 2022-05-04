3419a227d0a845b624ac4b1b0d6a18eaec62744d
SGrace11 <sgrace8@uncc.edu>
2022-05-04 17:25:48 -0400

    working through needed js

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,38 +1,8 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.keys')
-
-keys.addEventListener('click', e =>
-{if(e.target.matches('button'))
-    {
-        const key = e.target
-        const action = key.dataset.action
-
-        if(!action)
-        {
-            console.log('number key')
-        }
-        if(action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide')
-        {
-            console.log('operator key')
-        }
-        if(action === 'decimal')
-        {
-            console.log('decimal key')
-        }
-        if(action === 'clear')
-        {
-            console.log('clear key')
-        }
-        if(action === 'calculate')
-        {
-            console.log('equal key')
-        }
-    }
-})
-
 const display = document.querySelector('.display')
 
-keys.addEventListener('click', e => 
+keys.addEventListener('click', e =>
 {if(e.target.matches('button'))
     {
         const key = e.target
@@ -41,58 +11,110 @@ keys.addEventListener('click', e =>
         const displayedNum = display.textContent
         const previousKeyType = calculator.dataset.previousKeyType
 
-        Array.from(key.parentNode.children)
-            .forEach(k => k.classList.remove('is-depressed'))
+        if(action !== 'clear')
+        {
+            const clearButton = calculator.querySelector('[data-action=clear]')
+            clearButton.textContent = 'CE'
+        }
         if(!action)
         {
-            if(displayedNum === '0' || previousKeyType === 'operator')
+            console.log('number key')
+            if(displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate')
             {
                 display.textContent = keyContent
             } else
             {
                 display.textContent = displayedNum + keyContent
             }
-        }
-        if(action === 'decimal')
-        {
-            display.textContent = displayedNum + '.'
+            calculator.dataset.previousKeyType = 'number'
         }
         if(action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide')
         {
-            key.classList.add('is-depressed')
-            calculator.dataset.previousKeyType = 'operator'
-            calculator.dataset.firstValue = displayedNum
-            calculator.dataset.operator = action
-        }
-        if(action === 'calculate')
-        {
+            console.log('operator key')
             const firstValue = calculator.dataset.firstValue
             const secondValue = displayedNum
             const operator = calculator.dataset.operator
 
-            display.textContent = calculate(firstValue, operator, secondValue)
+            if(firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate')
+            {
+                const calcValue = calculate(firstValue, operator, secondValue)
+                display.textContent = calcValue
+
+                calculator.dataset.firstValue = calcValue
+            } else
+            {
+                calculator.dataset.firstValue = displayedNum
+            }
+
+            key.classList.add('is-depressed')
+            calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.operator = action
         }
+        if(action === 'decimal')
+        {
+            console.log('decimal key')
+            if(!displayedNum.includes('.'))
+            {
+                display.textContent = displayedNum + '.'
+            } else if(previousKeyType === 'operator' || previousKeyType === 'calculate')
+            {
+                display.textContent = '0.'
+            }
+            calculator.dataset.previousKeyType = 'decimal'
+        }
+        if(action === 'clear')
+        {
+            console.log('clear key')
+            if(key.textContent === 'AC')
+            {
+                calculator.dataset.firstValue = ''
+                calculator.dataset.modValue = ''
+                calculator.dataset.operator = ''
+                calculator.dataset.previousKeyType = ''
+            } else
+            {
+                key.textContent = 'AC'
+            }
+            display.textContent = '0'
+            calculator.dataset.previousKeyType = 'clear'
+        }
+        if(action === 'calculate')
+        {
+            console.log('equal key')
+            let firstValue = calculator.dataset.firstValue
+            let secondValue = displayedNum
+            const operator = calculator.dataset.operator
+
+            if(firstValue)
+            {
+                if(previousKeyType === 'calculate')
+                {
+                    firstValue = displayedNum
+                    secondValue = calculator.dataset.modValue
+                }
+                display.textContent = calculate(firstValue, operator, secondValue)
+            }
+            calculator.dataset.modValue = secondValue
+            calculator.dataset.previousKeyType = 'calculate'
+        }
+        Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
     }
 })
 
 const calculate = (n1, operator, n2) =>
 {
-    let result = ''
-
+    const firstNum = parseFloat(n1)
+    const secondNum = parseFloat(n2)
     switch(operator)
     {
         case 'add':
-            result = parseFloat(n1) + parseFloat(n2)
-            break;
+            return firstNum + secondNum
         case 'subtract':
-            result = parseFloat(n1) - parseFloat(n2)
-            break;
+            return firstNum - secondNum
         case 'multiply':
-            result = parseFloat(n1) * parseFloat(n2)
-            break;
+            return firstNum * secondNum
         case 'divide':
-            result = parseFloat(n1) / parseFloat(n2)
-            break;
+            return firstNum / secondNum
     }
-    return result
 }
