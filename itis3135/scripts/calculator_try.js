d866279e3f8c3e21df44190b9066dae65fafde6d
SGrace11 <sgrace8@uncc.edu>
2022-05-04 16:13:10 -0400

    coding 'happy path'

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -29,3 +29,70 @@ keys.addEventListener('click', e =>
         }
     }
 })
+
+const display = document.querySelector('.display')
+
+keys.addEventListener('click', e => 
+{if(e.target.matches('button'))
+    {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
+
+        Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
+        if(!action)
+        {
+            if(displayedNum === '0' || previousKeyType === 'operator')
+            {
+                display.textContent = keyContent
+            } else
+            {
+                display.textContent = displayedNum + keyContent
+            }
+        }
+        if(action === 'decimal')
+        {
+            display.textContent = displayedNum + '.'
+        }
+        if(action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide')
+        {
+            key.classList.add('is-depressed')
+            calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.firstValue = displayedNum
+            calculator.dataset.operator = action
+        }
+        if(action === 'calculate')
+        {
+            const firstValue = calculator.dataset.firstValue
+            const secondValue = displayedNum
+            const operator = calculator.dataset.operator
+
+            display.textContent = calculate(firstValue, operator, secondValue)
+        }
+    }
+})
+
+const calculate = (n1, operator, n2) =>
+{
+    let result = ''
+
+    switch(operator)
+    {
+        case 'add':
+            result = parseFloat(n1) + parseFloat(n2)
+            break;
+        case 'subtract':
+            result = parseFloat(n1) - parseFloat(n2)
+            break;
+        case 'multiply':
+            result = parseFloat(n1) * parseFloat(n2)
+            break;
+        case 'divide':
+            result = parseFloat(n1) / parseFloat(n2)
+            break;
+    }
+    return result
+}
