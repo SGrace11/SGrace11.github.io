dad9351967447166714c7ffd826c5b7449e44537
SGrace11 <sgrace8@uncc.edu>
2022-05-05 18:22:10 -0400

    assignment 14 js work

--- itis3135/Activity14/Grace-Activity14.js
+++ itis3135/Activity14/Grace-Activity14.js
@@ -1,3 +1,33 @@
-$(document).ready(function() {
-	
+$(document).ready(function()
+{
+	$('#nav_list a').click(function()
+    {
+        $.ajax({
+            url: 'json_files/' + $(this).attr('title') + '.json',
+            timeout: 10000,
+            error: function(xhr, status, error)
+            {
+                alert("Error: " + xhr.status + " - " + error);
+            },
+            dataType: 'json',
+            success: function(data)
+            {
+                $('main').html('');
+                $.each(data, function()
+                {
+                    $.each(this, function(key, value)
+                    {
+                        $('main').append
+                        (
+                            '<h1>' + value.title + '</h1>' +
+                            '<h2>' + value.month + '</h2>' +
+                            '<h3>' + value.speaker + '</h3>' +
+                            '<img src="' + value.image + '" alt = "' + $(this).attr('title') + '_picture"></img>' +
+                            '<p>' + value.text + '</p>'
+                        );
+                    });
+                });
+            }
+        });
+    });
 }); // end ready
