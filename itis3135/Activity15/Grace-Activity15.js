cfa09d39fa71066a4e3e0cba5850d126ba218580
SGrace11 <sgrace8@uncc.edu>
2022-05-05 19:02:04 -0400

    activity 15 js work

--- itis3135/Activity15/Grace-Activity15.js
+++ itis3135/Activity15/Grace-Activity15.js
@@ -1,3 +1,25 @@
-$(document).ready(function(){
-
+$(document).ready(function()
+{
+    $.ajax({
+        type: 'get',
+        url: 'facultyList.json',
+        dataType: 'json',
+        success: function(data)
+        {
+            $('#faculty').html('');
+            $.each(data, function()
+            {
+                $.each(this, function(key, value)
+                {
+                    $('#faculty').append
+                    (
+                        '<img src="' + value.image + '" alt="' + value.first_name +' picture">' +
+                        '<h2>' + value.full_name + '</h2>' +
+                        '<h3>' + value.department + '</h3>' +
+                        '<p>' + value.bio + '</p>'
+                    );
+                });
+            });
+        }
+    });
 });
