0904fbc7926007dddaec47257c61620a910e6a5b
SGrace11 <sgrace8@uncc.edu>
2023-02-21 18:24:28 -0500

    setting up routes and controller

--- itis4166/Project2/app.js
+++ itis4166/Project2/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
 const multer = require('multer');
+const eventRoutes = require('routes/eventRoutes');
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(methodOverride('_method'));
 app.get('/', (req, res)=> {
     res.render('index')
 });
+
+app.use('/events', eventRoutes);
 
 app.use((req, res, next)=> {
     let err = new Error('The server cannot locate ' + req.url);
