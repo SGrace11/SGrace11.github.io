083e9dbda7e59830707d8ed9ce72133deac3700c
SGrace11 <sgrace8@uncc.edu>
2023-02-21 18:25:47 -0500

    adding static to app

--- itis4166/Project2/app.js
+++ itis4166/Project2/app.js
@@ -10,6 +10,7 @@ let port = 3000;
 let host = 'localhost';
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(morgan.apply('tiny'));
 app.use(methodOverride('_method'));
